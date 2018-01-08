a4773a752e71a0b894bfe1d63e3a69bc8f58b494
qinkai <qinkai@tuhu.cn>
2018-01-08 10:49:18 +0800

    Hello World!

---
+++ src/HelloWorld.kt
@@ -0,0 +1,7 @@
+/**
+ * Created by Qin kai on 2018/1/8
+ */
+
+fun main(args: Array<String>) {
+    println("Hello world!")
+}
---
+++ src/first.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by Qin kai on 2018/1/8
+ */
+fun main(args: Array<String>) {
+    var max = 3
+    var min = 2
+
+    if (max > min) {
+        println("hihihi")
+    }
+}
