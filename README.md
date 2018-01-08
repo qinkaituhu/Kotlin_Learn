cc055bcfdab92fa2f0eb331f30ffa84b85821d17
qinkai <qinkai@tuhu.cn>
2018-01-08 11:11:19 +0800

    变量：var 常量：val

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Kotlin_Learn
 Learn Kotlin
+《第一季 Kotlin 崛起：次世代 Android 开发》
--- src/first.kt
+++ src/first.kt
@@ -5,7 +5,27 @@ fun main(args: Array<String>) {
     var max = 3
     var min = 2
 
+    val pai = 3.1415926
+    val 女朋友数量 = 1
+    val 工资 = 5000
+    var girlfrendcount = 12
+
+    var meals = 3
+    meals = meals -1
+    val k = meals * 350 * 365
+
     if (max > min) {
         println("hihihi")
     }
+    println(pai)
+    println(女朋友数量)
+    println(工资)
+    println(girlfrendcount)
+
+    println(k)
 }
+
+/** 总结
+ *  var -> 变量
+ *  val -> 常量
+ */
